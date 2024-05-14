e6209751ff242c98bf38f1db17f1bbaef774c826
Manuel Bahamóndez-Honores <manuel@bahamondez.com>
2024-05-14 15:11:18 -0400

    wip: generate list of arguments to pass downstream

--- aws/wrapper.ts
+++ aws/wrapper.ts
@@ -1,12 +1,12 @@
-import { tmpdir } from 'node:os';
-import { mkdtemp } from 'node:fs';
-import { sep } from 'node:path';
+import { tmpdir } from "node:os";
+import { mkdtemp } from "node:fs";
+import { sep } from "node:path";
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const tmpDir = tmpdir();
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 delete argv["_"];
 console.log("argv: ", argv);
@@ -16,7 +16,9 @@ if (!source) {
   throw new Error("Missing required attribute input-instance-folder");
 }
 if (!source.startsWith("s3://")) {
-  throw new Error("Input instance folder should point to a location in s3. Example: s3://bucket_name/key/");
+  throw new Error(
+    "Input instance folder should point to a location in s3. Example: s3://bucket_name/key/",
+  );
 }
 if (!source.endsWith("/")) {
   throw new Error("Input instance folder should end with a trailing slash");
@@ -24,8 +26,21 @@ if (!source.endsWith("/")) {
 
 mkdtemp(`${tmpDir}${sep}`, async (err, directory) => {
   if (err) throw err;
-  console.log(directory);
+  const newArgs = { ...argv };
+  newArgs["input-instance-folder"] = directory;
 
-  const output = await exec(`aws s3 cp ${source} ${directory} --recursive`);
-  console.log({output});
+  const args: string[] = [];
+
+  for (const key of Object.keys(newArgs)) {
+    args.push(`--${key}=${newArgs[key]}`);
+  }
+
+  const cmd = `Cell2Fire ${args.join(" ")}`;
+  console.log({ cmd });
+
+  const s3Copy = await exec(`aws s3 cp ${source} ${directory} --recursive`);
+  console.log({ s3Copy });
+
+  const output = await exec(cmd);
+  console.log({ output });
 });
